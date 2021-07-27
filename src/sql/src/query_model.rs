0e0d1d9a4a4bacb2981d2601e48376605fcd3b31
Andres Senac <andres@senac.es>
2021-07-27 11:21:08 +0200

    add todo

--- src/sql/src/query_model.rs
+++ src/sql/src/query_model.rs
@@ -1587,6 +1587,8 @@ mod tests {
             "select a.b from a as a(a,b) full join b as b(b, c) on a.a",
             "select a.b from a as a(a,b) full join b as b(b, c) on (select a.a from c as c(c, d))",
             "select a.* from (values((select column1 from b), (select column2 from c))) as a(a, b)",
+            // @todo this should fail with ambiguous column name error
+            "select b as a, a from a as a(a, b) order by a",
         ];
         for test_case in test_cases {
             let parsed = parse_statements(test_case).unwrap();

c50b0e17da341933dd5bd9fb936d94a816ce54d9
Andres Senac <asenac@materialize.com>
2021-10-11 13:58:59 +0200

    broken for MirRelationExpr

--- src/transform/src/rewrite_engine.rs
+++ src/transform/src/rewrite_engine.rs
@@ -42,10 +42,39 @@ pub trait MutExprVisitor<Expr: VisitMutExpr, Data: Default> {
     fn post_visit(&self, _expr: &mut Expr, _data: &mut Data) {}
 }
 
+pub trait Transform<Expr> {
+    fn apply_transforms(&self, expr: &mut Expr);
+}
+
+struct SingleTransform<Expr, Data> {
+    transform: Box<dyn MutExprVisitor<Expr, Data>>,
+}
+
+impl<Expr, Data> Transform<Expr> for SingleTransform<Expr, Data>
+where
+    Data: Default,
+    Expr: VisitMutExpr,
+{
+    /// Apply the list of tranformations to the given expression.
+    fn apply_transforms(&self, expr: &mut Expr) {
+        let data = RefCell::new(Default::default());
+        expr.visit_mut_pre_post_in(
+            &mut |e| self.transform.pre_visit(e, &mut data.borrow_mut()),
+            &mut |e, input_idx| {
+                self.transform
+                    .in_visit(e, input_idx, &mut data.borrow_mut());
+            },
+            &mut |e| {
+                self.transform.post_visit(e, &mut data.borrow_mut());
+            },
+        );
+    }
+}
+
 /// A list of transformations, represented as `MutExprVisitor`, that can be
 /// applied during the same graph traversal. They all must accept the same
 /// data type.
-struct Transforms<Expr, Data>
+struct CombinedTransforms<Expr, Data>
 where
     Data: Default,
     Expr: VisitMutExpr,
@@ -53,13 +82,13 @@ where
     transforms: Vec<Box<dyn MutExprVisitor<Expr, Data>>>,
 }
 
-impl<Expr, Data> Transforms<Expr, Data>
+impl<Expr, Data> Transform<Expr> for CombinedTransforms<Expr, Data>
 where
     Data: Default,
     Expr: VisitMutExpr,
 {
     /// Apply the list of tranformations to the given expression.
-    fn apply(&self, expr: &mut Expr) {
+    fn apply_transforms(&self, expr: &mut Expr) {
         let data = RefCell::new(Default::default());
         expr.visit_mut_pre_post_in(
             &mut |e| {
@@ -78,5 +107,75 @@ where
                 }
             },
         );
+    }
+}
+
+use expr::MirRelationExpr;
+
+impl VisitMutExpr for MirRelationExpr {
+    fn visit_mut_pre_post_in<FnPre, FnPost, FnIn>(
+        &mut self,
+        pre: &mut FnPre,
+        f_in: &mut FnIn,
+        post: &mut FnPost,
+    ) where
+        FnPre: FnMut(&mut Self),
+        FnIn: FnMut(&Self, usize),
+        FnPost: FnMut(&mut Self),
+    {
+        pre(self);
+
+        match self {
+            MirRelationExpr::Constant { .. } | MirRelationExpr::Get { .. } => (),
+            MirRelationExpr::Let { value, body, .. } => {
+                value.visit_mut_pre_post_in(pre, f_in, post);
+                f_in(self, 1);
+                body.visit_mut_pre_post_in(pre, f_in, post);
+            }
+            MirRelationExpr::Project { input, .. } => {
+                input.visit_mut_pre_post_in(pre, f_in, post);
+            }
+            MirRelationExpr::Map { input, .. } => {
+                input.visit_mut_pre_post_in(pre, f_in, post);
+            }
+            MirRelationExpr::FlatMap { input, .. } => {
+                input.visit_mut_pre_post_in(pre, f_in, post);
+            }
+            MirRelationExpr::Filter { input, .. } => {
+                input.visit_mut_pre_post_in(pre, f_in, post);
+            }
+            MirRelationExpr::Join { inputs, .. } => {
+                let num_inputs = inputs.len();
+                for (input_idx, input) in inputs.iter_mut().enumerate() {
+                    input.visit_mut_pre_post_in(pre, f_in, post);
+                    if input_idx + 1 < num_inputs {
+                        f_in(self, input_idx + 1);
+                    }
+                }
+            }
+            MirRelationExpr::Reduce { input, .. } => {
+                input.visit_mut_pre_post_in(pre, f_in, post);
+            }
+            MirRelationExpr::TopK { input, .. } => {
+                input.visit_mut_pre_post_in(pre, f_in, post);
+            }
+            MirRelationExpr::Negate { input } => input.visit_mut_pre_post_in(pre, f_in, post),
+            MirRelationExpr::Threshold { input } => input.visit_mut_pre_post_in(pre, f_in, post),
+            MirRelationExpr::Union { base, inputs } => {
+                base.visit_mut_pre_post_in(pre, f_in, post);
+                for (input_idx, input) in inputs.iter_mut().enumerate() {
+                    f_in(self, input_idx + 1);
+                    input.visit_mut_pre_post_in(pre, f_in, post);
+                }
+            }
+            MirRelationExpr::ArrangeBy { input, .. } => {
+                input.visit_mut_pre_post_in(pre, f_in, post);
+            }
+            MirRelationExpr::DeclareKeys { input, .. } => {
+                input.visit_mut_pre_post_in(pre, f_in, post);
+            }
+        }
+
+        post(self);
     }
 }
